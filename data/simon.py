fc472fbdf060664f1a3d5dd79d484f6254149502
hoyohoyo9 <kelly.cody@gmail.com>
2014-08-05 15:16:40 -0500

    Cleaned up some code

--- data/simon.py
+++ data/simon.py
@@ -6,15 +6,16 @@ right = pygame.mixer.Sound('data/sfx/right.ogg')
 wrong = pygame.mixer.Sound('data/sfx/wrong.ogg')
 
 class State(object):
-    def __init__(self):
+    def __init__(self, simon):
         self.active = False
+        self.simon = simon
     def set_next_state(self, state):
         self.next_state = state
 
 class OutputState(State):
-    def __init__(self, rounds):
-        State.__init__(self)
-        self.moves = rounds
+    def __init__(self, simon):
+        State.__init__(self, simon)
+        self.moves = simon.rounds+simon.addedRounds
         self.moveList = []
         for i in range(0, self.moves):
             self.moveList.append(random.randint(0,3))
@@ -25,7 +26,7 @@ class OutputState(State):
         self.startDelayFrame = 0
     def set_next_state(self, state):
         self.next_state = state
-    def update(self, events, simon):
+    def update(self, events):
         if self.startDelayFrame < self.startDelay:
             self.startDelayFrame += 1
         else:
@@ -33,89 +34,74 @@ class OutputState(State):
                 self.delayFrame -= 1
             else:
                 if self.currentmove < self.moves:
-                    simon.board.flash_tile(self.moveList[self.currentmove])
+                    self.simon.board.flash_tile(self.moveList[self.currentmove])
                     self.currentmove += 1
                 else:
                     self.active = False
                     self.next_state.active = True
-                    simon.moveList = self.moveList
+                    self.simon.moveList = self.moveList
                 self.delayFrame = self.delay
 
 class InputState(State):
-    def __init__(self):
-        State.__init__(self)
+    def __init__(self, simon):
+        State.__init__(self, simon)
         self.delayFrames = 0
         self.delay = 250
         self.moveList = []
-    def update(self, events, simon):
+    def update(self, events):
         if not self.moveList == []:
             if self.delayFrames < self.delay:
                 self.delayFrames += 1
                 for event in events:
                     if event.type == pygame.KEYDOWN:
                         if event.key == pygame.K_q:
-                            simon.board.flash_tile(0)
-                            self.moveList.append(0)
-                            self.delayFrames = 0
+                            self.activateTile(0)
                         if event.key == pygame.K_w:
-                            simon.board.flash_tile(1)
-                            self.moveList.append(1)
-                            self.delayFrames = 0
+                            self.activateTile(1)
                         if event.key == pygame.K_a:
-                            simon.board.flash_tile(2)
-                            self.moveList.append(2)
-                            self.delayFrames = 0
+                            self.activateTile(2)
                         if event.key == pygame.K_s:
-                            simon.board.flash_tile(3)
-                            self.moveList.append(3)
-                            self.delayFrames = 0
+                            self.activateTile(3)
                     if event.type == pygame.MOUSEBUTTONDOWN:
                         mousePos = pygame.mouse.get_pos()
-                        tile = simon.board.get_tile(mousePos)
-                        simon.board.flash_tile(tile)
-                        self.moveList.append(tile)
-                        self.delayFrames = 0
+                        tile = self.simon.board.get_tile(mousePos)
+                        self.activateTile(tile)
             else:
                 self.active = False
                 self.next_state.active = True
-                simon.playerMoveList = self.moveList
+                self.simon.playerMoveList = self.moveList
         else:
             for event in events:
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     mousePos = pygame.mouse.get_pos()
-                    tile = simon.board.get_tile(mousePos)
-                    simon.board.flash_tile(tile)
-                    self.moveList.append(tile)
+                    tile = self.simon.board.get_tile(mousePos)
+                    self.activateTile(tile)
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_q:
-                        simon.board.flash_tile(0)
-                        self.moveList.append(0)
-                        self.delayFrames = 0
+                        self.activateTile(0)
                     if event.key == pygame.K_w:
-                        simon.board.flash_tile(1)
-                        self.moveList.append(1)
-                        self.delayFrames = 0
+                        self.activateTile(1)
                     if event.key == pygame.K_a:
-                        simon.board.flash_tile(2)
-                        self.moveList.append(2)
-                        self.delayFrames = 0
+                        self.activateTile(2)
                     if event.key == pygame.K_s:
-                        simon.board.flash_tile(3)
-                        self.moveList.append(3)
-                        self.delayFrames = 0
+                        self.activateTile(3)
+    def activateTile(self, tile):
+        self.simon.board.flash_tile(tile)
+        self.moveList.append(tile)
+        self.delayFrames = 0
 
 class ResultState(State):
-    def __init__(self):
-        State.__init__(self)
-    def update(self, events, simon):
-        if simon.moveListsMatch():
+    def __init__(self, simon):
+        State.__init__(self, simon)
+    def update(self, events):
+        if self.simon.moveListsMatch():
             right.play()
-            simon.board.flash()
-            simon.addRound()
+            self.simon.board.flash()
+            self.simon.addRound()
         else:
             wrong.play()
-            simon.board.disable()
-            simon.reset()
+            self.simon.board.disable()
+            self.simon.reset()
 
 class Simon(object):
     def __init__(self, board, rounds, addedRounds):
@@ -123,9 +109,9 @@ class Simon(object):
         self.rounds = rounds
         self.addedRounds = addedRounds
         self.stateDict = [
-            OutputState(self.rounds+self.addedRounds),
-            InputState(),
-            ResultState()
+            OutputState(self),
+            InputState(self),
+            ResultState(self)
         ]
         for i in range(0,len(self.stateDict)-1):
             self.stateDict[i].set_next_state(self.stateDict[i+1])
@@ -135,7 +121,7 @@ class Simon(object):
     def update(self, events):
         for state in self.stateDict:
             if state.active:
-                state.update(events, self)
+                state.update(events)
     def moveListsMatch(self):
         return self.moveList == self.playerMoveList
     def reset(self):
